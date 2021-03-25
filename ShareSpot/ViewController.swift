4531c832f4b3a85e54e639f23f5efaa9a87761eb
Joshua Gray <jgray51@vols.utk.edu>
2021-03-24 23:30:17 -0400

    Initial Commit

---
+++ ShareSpot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShareSpot
+//
+//  Created by Joshua Gray on 3/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
