af0a1673a8304f6018e0c8f812aba2d5e820052f
Joshua Gray <jgray51@vols.utk.edu>
2021-04-06 23:24:52 -0400

    Added Camera functionality

--- ShareSpot/ViewController.swift
+++ ShareSpot/ViewController.swift
@@ -17,8 +17,11 @@ class ViewController: UIViewController {
 
             // Automatically sign in the user.
             GIDSignIn.sharedInstance()?.restorePreviousSignIn()
-
-
+        
+        imageView.backgroundColor = .secondarySystemBackground
+        button.backgroundColor = .systemGray
+        button.setTitle("Take Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
     }
     
     @IBAction func didTapSignOut(_ sender: AnyObject) {
@@ -26,6 +29,38 @@ class ViewController: UIViewController {
         // [START_EXCLUDE silent]
         // [END_EXCLUDE]
       }
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton!
+    
+    @IBAction func didTapButton(){
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+
+}
+
+extension ViewController: UIImagePickerControllerDelegate,
+                         UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
+                UIImage else {
+                return
+            }
+        
+        imageView.image = image
+    }
+  
 
 }
 
